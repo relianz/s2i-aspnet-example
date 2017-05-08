947de2a4d16d4fbcff647a23561cde6817550f7c
relianz <markus@stulle.zone>
2017-05-08 07:00:52 +0200

    Update Program.cs
    Removed superfluous imports.

--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.IO;
+// using System.Linq;
+// using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
