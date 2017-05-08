1d0e00ab01780d508a536f10bc805d1b57a1fda2
relianz <markus@stulle.zone>
2017-05-08 08:09:38 +0200

    Update Program.cs
    Finally it compiles on OpenShift.

--- app/Program.cs
+++ app/Program.cs
@@ -1,10 +1,5 @@
-// using System.Linq;
-// using System.Threading.Tasks;
-
-// using System;                     
-using System.IO;                            
-// using System.Collections.Generic;
-
+                    
+using System.IO;                            // Directory.
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
