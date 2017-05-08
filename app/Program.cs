5287c833ef93526356df8d55ec83081b253ee879
relianz <markus@stulle.zone>
2017-05-08 07:08:24 +0200

    Update Program.cs
    Reference to System.IO required for class Directory.

--- app/Program.cs
+++ app/Program.cs
@@ -1,9 +1,9 @@
 // using System;
 // using System.Collections.Generic;
-// using System.IO;
 // using System.Linq;
 // using System.Threading.Tasks;
 
+using System.IO;                            // Directory.
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
