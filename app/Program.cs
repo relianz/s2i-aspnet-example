d15ac2ebbb67239cf85a1f3ae0abe8ac3e43cd22
relianz <markus@stulle.zone>
2017-05-08 07:18:05 +0200

    Update Program.cs
    Added reference to namepace System again.

--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,8 @@
-// using System;
 // using System.Collections.Generic;
 // using System.Linq;
 // using System.Threading.Tasks;
 
+using System;                     
 using System.IO;                            // Directory.
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
